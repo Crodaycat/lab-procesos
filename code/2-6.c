d1e6c7a90618570ffb8db8355b6307477821c518
Luis Giraldo <luis.giraldo10@udea.edu.co>
2020-04-18 23:36:02 -0500

    FEAT: Código punto 6

---
+++ code/2-6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h> 
+#include <sys/wait.h>
+
+int main(void) {
+
+  int pid = fork();
+
+  if (pid != 0) {
+    int wRes = waitpid(pid,0, 0);
+    printf("Goodbye, wait: %d\n", wRes);
+  } else {
+    printf("Hello\n");
+  }
+
+  return 0;
+}
